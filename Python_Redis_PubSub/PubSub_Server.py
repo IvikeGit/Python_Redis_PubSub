49973d74da06a9178a557114240461bb7c6958a0
Kollár Iván <ivikemail@gmail.com>
2016-04-27 14:06:00 +0200

    remove

--- Python_Redis_PubSub/PubSub_Server.py
+++ Python_Redis_PubSub/PubSub_Server.py
@@ -10,18 +10,22 @@ class ChannelService:
     _rdb = None
     
     def __init__(self):
+        # setup the logging    
+        logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+        # initaite db
         self._rdb = redis.StrictRedis(host = settings.SERVER_HOST, port = settings.SERVER_PORT, password = settings.SERVER_PASSWORD, db=0)
 
     def runChannel(self):
          try:
             counter = 0
+            logging.debug('Running redis service on port :%s host: %s', settings.SERVER_HOST, settings.SERVER_PORT)
 
-            while counter <= 1000:
-                time.sleep(0.5)
+            while counter <= 60:
+                time.sleep(1)
                 counter +=1
                 message = '{0} published on "test" channel'.format(counter)
                 self._rdb.publish('test', message);
-                if settings.DEBUG: print message
+                logging.debug(message)
 
             self._rdb.publish('test', 'KILL');
 
