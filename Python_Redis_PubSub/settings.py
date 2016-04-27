18b1ee19d2c4c69daf51c05b98497b7305a247e2
Kollár Iván <ivikemail@gmail.com>
2016-04-27 10:37:54 +0200

    remove client

--- Python_Redis_PubSub/settings.py
+++ Python_Redis_PubSub/settings.py
@@ -53,10 +53,13 @@ SERVER_PORT = 6379
 SERVER_PASSWORD = 'Valami123456'
 
 if not DEBUG:
-    sys_json = os.getenv('VCAP_SERVICES')
-    SERVER_HOST = sys_json['redis-1'][0]['credentials']['host']
-    SERVER_PORT = sys_json['redis-1'][0]['credentials']['port']
-    SERVER_PASSWORD = sys_json['redis-1'][0]['credentials']['password']
+    SERVER_HOST = '10.72.6.22'
+    SERVER_PORT = 46415
+    SERVER_PASSWORD = '4d4a1000-f1c5-446c-b730-45a44edd3e6d'
+    #sys_json = os.getenv('VCAP_SERVICES')
+    #SERVER_HOST = sys_json['redis-1'][0]['credentials']['host']
+    #SERVER_PORT = sys_json['redis-1'][0]['credentials']['port']
+    #SERVER_PASSWORD = sys_json['redis-1'][0]['credentials']['password']
 
 
 
